562e0afbc8ae5c480118172cb83620d61f074de5
Radek <radoslaw.grzymala@hotmail.com>
2024-09-17 23:18:19 +0200

    style: added usage of periodic elements list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PeriodicElementsListComponent} from './components/periodic-elements/periodic-elements-list/periodic-elements-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PeriodicElementsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
